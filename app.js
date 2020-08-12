460583e86a91900d92bd2e86209f44a867d47b59
Zenzi Ali <zeeathome935@gmail.com>
2020-08-12 16:04:38 -0400

    refactoring

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
+/* eslint-disable no-console */
 const cors = require('cors');
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
 
-app.use(morgan("common"));
+app.use(morgan('common'));
 app.use(cors());
 
 //create a variable for data
@@ -14,29 +15,29 @@ app.get('/apps', (req, res) => {
   //assign const to sort and genres
   const {sort, genre} = req.query;
   //validating query entries
- // if genre && != stuffonlist then throw error
- if(genre && !validGenres.includes(genre)) {
-   res.status(400).send(`genre must be one of ${validGenres}`)
- };
- if(sort && !validSort.includes(sort)) {
-   res.status(400).send(`sort must be ${validSort}`)
- };
- const filteredArray = [...playApps];
- if(genre) {
-   filteredArray = playApps.filter(app => {
-     return app.Genres === genre 
-   })
- }
- if(sort) {
-   filteredArray.sort((a, b) => {
-     return a[sort] - b[sort];
-   })
- }
+  // if genre && != stuffonlist then throw error
+  if(genre && !validGenres.includes(genre)) {
+    res.status(400).send(`genre must be one of ${validGenres}`);
+  }
+  if(sort && !validSort.includes(sort)) {
+    res.status(400).send(`sort must be ${validSort}`);
+  }
+  let filteredArray = [...playApps];
+  if(genre) {
+    filteredArray = playApps.filter(app => {
+      return app.Genres === genre; 
+    });
+  }
+  if(sort) {
+    filteredArray.sort((a, b) => {
+      return a[sort] - b[sort];
+    });
+  }
 
- //
- // filter genre list by given name
+  //
+  // filter genre list by given name
   //display data to client
   res.json(filteredArray);
 });
 
-app.listen(8000, () => {console.log('Server started on port:8000')});
+app.listen(8000, () => {console.log('Server started on port:8000');});
